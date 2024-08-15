94279031b7f1cd3f13794aaa6c8afd90d2f3f107
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-15 02:36:41 -0500

    Create signUp mutation function in user.resolver.js file.

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,6 +1,47 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
 
 const userResolver = {
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required");
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("Username already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // https://avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (err) {
+        console.error("Error in signUp: ", err);
+        throw new Error(err.message || "Internal server error");
+      }
+    }, // End of signUp 
+
+
+  },
   Query: {
     users: (_, __, { req, res }) => {
       return users;
@@ -9,7 +50,6 @@ const userResolver = {
       return users.find((user) => user._id === userId);
     }
   },
-  Mutation: {},
 };
 
 export default userResolver;
