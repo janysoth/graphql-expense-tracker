08fd622ed871bd6d79962bf6dada468cabfcdc4e
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-17 08:43:00 -0500

    Create NotFoundPage.

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,8 +1,27 @@
-
-const NotFoundPage = () => {
+const NotFound = () => {
   return (
-    <div>NotFoundPage</div>
+    <section>
+      <div className=' text-white'>
+        <div className='flex h-screen'>
+          <div className='m-auto text-center'>
+            <div>
+              <a href="/">
+                <img src='/404.svg' alt='404' />
+              </a>
+            </div>
+            <p className='text-sm md:text-base text-[#F6009B] p-2 mb-4'>
+              {"The stuff you were looking for doesn't exist"}
+            </p>
+            <a
+              href='/'
+              className='bg-transparent hover:bg-[#F6009B] text-[#F6009B] hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-[#F6009B] hover:border-transparent'
+            >
+              Take me home
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
-
-export default NotFoundPage;
+export default NotFound;
