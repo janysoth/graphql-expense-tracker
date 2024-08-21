38297f31a67a9a9fb7819493589aa2e81315d561
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-21 03:07:26 -0500

    Create handleDelete function to delete a transaction in Card component.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,7 +5,11 @@ import { FaSackDollar } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { HiPencilAlt } from "react-icons/hi";
 import { Link } from "react-router-dom";
+
 import { formatDate } from "../utils/formatDate";
+import toast from 'react-hot-toast';
+import { useMutation } from '@apollo/client';
+import { DELETE_TRANSACTION } from '../../graphql/mutations/transaction.mutation';
 
 const categoryColorMap = {
   saving: "from-green-700 to-green-400",
@@ -16,8 +20,10 @@ const categoryColorMap = {
 
 const Card = ({ transaction }) => {
   let { category, amount, location, date, paymentType, description } = transaction;
-
   const cardClass = categoryColorMap[category];
+  const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
+    refetchQueries: ["GetTransactions"],
+  });
 
   // Capitalize the first letter of the description
   description = description[0]?.toUpperCase() + description.slice(1);
@@ -26,13 +32,24 @@ const Card = ({ transaction }) => {
 
   const formattedDate = formatDate(date);
 
+  const handleDelete = async () => {
+    try {
+      await deleteTransaction({ variables: { transactionId: transaction._id } });
+      toast.success("Transaction has been deleted successfully.");
+    } catch (error) {
+      console.error("Error in deleting transaction: ", error);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className={`rounded-md p-4 bg-gradient-to-br ${cardClass}`}>
       <div className='flex flex-col gap-3'>
         <div className='flex flex-row items-center justify-between'>
           <h2 className='text-lg font-bold text-white'>{category}</h2>
           <div className='flex items-center gap-2'>
-            <FaTrash className={"cursor-pointer"} />
+            {!loading && <FaTrash className={"cursor-pointer"} onClick={handleDelete} />}
+            {loading && <div className='w-6 h-6 border-t-2 border-b-2  rounded-full animate-spin'></div>}
             <Link to={`/transaction/123`}>
               <HiPencilAlt className='cursor-pointer' size={20} />
             </Link>
