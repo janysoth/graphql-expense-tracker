d10cb77c87b6a2cf1c0874101b6a61141f8364c1
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-24 03:04:38 -0500

    Set up the project for development.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import path from 'path';
 import passport from 'passport';
 import session from 'express-session';
 import connectMongo from 'connect-mongodb-session';
@@ -23,6 +23,7 @@ dotenv.config();
 configurePassport();
 
 const app = express();
+const __dirname = path.resolve();
 
 const httpServer = http.createServer(app);
 
@@ -82,6 +83,13 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+// npm run build will build the frontend app, and it will be the optimized version of the app
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 // Modified server start up
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
