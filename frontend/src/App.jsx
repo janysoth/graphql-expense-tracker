10131bdce0f0731f4a694ce6601fa6bb3f661478
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-26 23:13:54 -0500

    Fix some typos in App.jsx file in frontend folder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,12 +18,12 @@ function App() {
     <>
       {data?.authUser && <Header />}
       <Routes>
-        <Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
-        <Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
-        <Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
+        <Route path='/' element={data?.authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path='/login' element={!data?.authUser ? <LoginPage /> : <Navigate to='/' />} />
+        <Route path='/signup' element={!data?.authUser ? <SignUpPage /> : <Navigate to='/' />} />
         <Route
           path='/transaction/:id'
-          element={data.authUser ? <TransactionPage /> : <Navigate to='/login' />}
+          element={data?.authUser ? <TransactionPage /> : <Navigate to='/login' />}
         />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
