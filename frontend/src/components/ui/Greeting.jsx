b70a7eb762b715ca2dddd08fac3fc31ae24c8373
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-29 00:24:39 -0500

    Update Greeting and LoginPage components with new styles and layout.

--- frontend/src/components/ui/Greeting.jsx
+++ frontend/src/components/ui/Greeting.jsx
@@ -1,10 +1,10 @@
 const Greeting = () => {
   return (
-    <div className='mb-2'>
-      <h1 className='md:text-4xl text-8xl lg:text-6xl font-bold text-center  relative z-50 text-white pt-10'>
+    <div className='mb-2 p-4 sm:p-6 md:p-8 lg:p-10'>
+      <p className="text-3xl sm:text-4xl font-semibold mb-2 text-white text-center">
         Welcome to Expense Tracker
-      </h1>
-      <p className='md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text p-5'>
+      </p>
+      <p className='text-2xl sm:text-3xl md:text-4xl font-bold text-center relative z-50 mb-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 text-transparent bg-clip-text p-2 sm:p-4 block'>
         Spend wisely, track wisely
       </p>
     </div>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -36,55 +36,54 @@ const LoginPage = () => {
   };
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen'>
+    <div className="flex flex-col justify-center items-center min-h-screen p-4 sm:p-8">
       <Greeting />
-      <div className='flex rounded-lg overflow-hidden z-50 bg-gray-300'>
-        <div className='w-full bg-gray-100 min-w-80 sm:min-w-96 flex items-center justify-center'>
-          <div className='max-w-md w-full p-6'>
-            <h1 className='text-3xl font-semibold mb-6 text-black text-center'>Login</h1>
-            <h1 className='text-sm font-semibold mb-6 text-gray-500 text-center'>
-              Welcome back! Log in to your account
-            </h1>
-            <form className='space-y-4' onSubmit={handleSubmit}>
-              <InputField
-                label='Username'
-                id='username'
-                name='username'
-                value={loginData.username}
-                onChange={handleChange}
-              />
-
-              <InputField
-                label='Password'
-                id='password'
-                name='password'
-                type='password'
-                value={loginData.password}
-                onChange={handleChange}
-              />
-              <div>
-                <button
-                  type='submit'
-                  className='w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed
-									'
-                  disabled={loading}
-                >
-                  {loading ? "Loading..." : "Login"}
-                </button>
-              </div>
-            </form>
-            <div className='mt-4 text-sm text-gray-600 text-center'>
-              <p>
-                {"Don't"} have an account?{" "}
-                <Link to='/signup' className='text-black hover:underline'>
-                  Sign Up
-                </Link>
-              </p>
-            </div>
+      <div className="w-full max-w-sm sm:max-w-md bg-white rounded-lg shadow-lg p-6">
+        <h1 className="text-2xl sm:text-3xl font-semibold mb-4 text-black text-center">
+          Login
+        </h1>
+        <h2 className="text-sm sm:text-base font-semibold mb-6 text-gray-500 text-center">
+          Welcome back! Log in to your account
+        </h2>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <InputField
+            label="Username"
+            id="username"
+            name="username"
+            value={loginData.username}
+            onChange={handleChange}
+            disabled={loading}
+          />
+          <InputField
+            label="Password"
+            id="password"
+            name="password"
+            type="password"
+            value={loginData.password}
+            onChange={handleChange}
+            disabled={loading}
+          />
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={loading}
+            >
+              {loading ? "Loading..." : "Login"}
+            </button>
           </div>
+        </form>
+        <div className="mt-4 text-sm sm:text-base text-gray-600 text-center">
+          <p>
+            {"Don't"} have an account?{" "}
+            <Link to="/signup" className="text-black hover:underline">
+              Sign Up
+            </Link>
+          </p>
         </div>
       </div>
     </div>
   );
 };
+
 export default LoginPage;
